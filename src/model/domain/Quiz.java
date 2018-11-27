d24673337d82ffd3b2d3c4d439a009ba29afb53b
david <davidvc@gmail.com>
2018-11-27 13:54:05 +0100

    removed deprecated classes

--- src/model/domain/Quiz.java
+++ src/model/domain/Quiz.java
@@ -1,17 +1,12 @@
 package model.domain;
-import model.db.QuestionTextReader;
 import java.util.List;
 
-import model.db.CategorieTextReader;
 import model.db.DbStrategy;
-import model.db.QuestionTextReader;
 import model.db.TextDbCategorieReader;
 import model.db.TextDbQuestionReader;
 
 import model.domain.Question;
 public class Quiz {
-	private CategorieTextReader CR;
-	private QuestionTextReader QR;
 	private DbStrategy categorieReader;
 	private DbStrategy questionReader;
 	
@@ -50,13 +45,13 @@ public class Quiz {
 	//TODO: add more methods, needed for story 1-4
 	
 	public void runQuiz() {
-		QuestionTextReader qr = new QuestionTextReader();
+		/*QuestionTextReader qr = new QuestionTextReader();
 		for (Question q : qr.getQuestions()) {
 			for (String s : q.getStatements()) {
 				System.out.println(q.getQuestion() + ' ' + s);
 			}
 		}
-		System.out.println();
+		System.out.println();*/
 		
 	}
 }
