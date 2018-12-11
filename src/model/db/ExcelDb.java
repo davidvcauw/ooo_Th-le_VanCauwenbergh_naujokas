6b97f9bd828640a9a739ba8497b94a28a52568c0
Audrius Naujokas <audrius.naujokas@gmail.com>
2018-12-11 14:26:32 +0100

    a

--- src/model/db/ExcelDb.java
+++ src/model/db/ExcelDb.java
@@ -1,18 +1,37 @@
 package model.db;
 
 import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import excel.ExcelPlugin;
+import jxl.read.biff.BiffException;
+import jxl.write.WriteException;
 
 public class ExcelDb<E> implements DbStrategy<E>{
 
+	private ExcelPlugin el;
 	@Override
 	public void load() {
-		// TODO Auto-generated method stub
 		
-	}
+		try{
+			ArrayList <ArrayList<String>> list = ExcelPlugin.read(new File("Questions.xls"), 1);
+			for (int i = 0; i < list.size();i++){
+				ArrayList<String>rijtje = list.get(i);
+				for (int j=0;j<rijtje.size();j++){
+					System.out.println(rijtje.get(j));
+				}
+			}
+		}
+		catch (Exception e){
+			System.out.println("lezen excel "+ e.getMessage());
+		}
+	}	
 
 	@Override
-	public void save() {
-		// TODO Auto-generated method stub
+	public void save() throws WriteException, IOException {
+		 el = new ExcelPlugin(new File("Questions.xls"));
+		el.write();
 		
 	}
 
